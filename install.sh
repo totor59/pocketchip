62a08de9826363aea91f0f2983dd8976059eb5ba
Victor Marechal <vmarechal@adwanted.com>
2017-07-21 11:43:37 +0200

    install.sh. vim git et tmux config

---
+++ install.sh
@@ -0,0 +1,39 @@
+###############################################################################
+# POCKETCHIP CONFIGURATION
+# 2017 totor59 <victormarechal59@gmail.com>
+# You have to run as root
+###############################################################################
+
+
+# GENERAL
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y install git-core vim-nox tmux python-pip mutt tree
+git clone https://github.com/totor59/pocketchip
+cd pocketchip
+
+# POCKETCHIP SETTINGS
+# Includes keymap for pocketchip and support for module rt2800usb
+mv pocketchip.kmap /home/chip/.config/
+rm /etc/rc.local
+cp rc.local /etc/
+cp battery /usr/bin/battery
+chmod 755 battery
+
+# GIT
+mv gitconfig /home/chip/.gitconfig
+
+# VIM
+chmod -R 777 /etc/vim
+mkdir bundle
+rm /etc/vim/vimrc
+mv vimrc /etc/vim/
+mkdir /etc/vim/bundle
+source /etc/vim/vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git /etc/vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+# TMUX
+cp tmux.conf /home/chip/.tmux.conf
+
